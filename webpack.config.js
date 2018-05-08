f65867d2b0f24f9315d6e701d61b0713cc7c105a
scyuan <644397334@qq.com>
2018-05-08 10:54:00 +0800

    2018-05-08 10:53:59

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     publicPath: '/dist/',
     // filename: 'build.js'
     filename : 'vue2-keyboard.js',
-    library: 'Vue2Keyboard', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
+    library: 'Vue2Keyboard',  // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true
   },
